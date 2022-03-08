ba5f6879c7c998a5e2412ccdab85528394422f7b
prozario1 <44505881+prozario1@users.noreply.github.com>
2022-03-08 11:50:43 -0500

    doodlejump

---
+++ client/doodle/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('grid');
+  const doodler = document.querySelector('div');
+
+  function createDoodler() {
+    grid.appendChild(doodler);
+    doodler.classList.add('doodler');
+    doodlerLeftSpace = platforms[0].left;
+    doodler.style.left = `${doodlerLeftSpace}px`;
+    doodler.style.bottom = `${doodlerBottomSpace}px`;
+  }
+});
