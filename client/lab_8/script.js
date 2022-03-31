a8d3298ee1746bdb3561f39dadc439b0a6511d0b
prozario1 <44505881+prozario1@users.noreply.github.com>
2022-03-31 16:37:14 -0400

    Update script.js

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -69,7 +69,8 @@ async function mainEvent() { // the async keyword means we can make API requests
 
   console.log(storedDataArray);
   //   const arrayFromJson = {data: []}; // TODO REMOVE
-  if (storedDataArray.length > 0) { // prevents race condition on data load
+  if (storedDataArray.length != null && storedDataArray.length > 0) {
+    // prevents race condition on data load
     submit.style.display = 'block';
     let currentArray = [];
     resto.addEventListener('input', async (event) => {
